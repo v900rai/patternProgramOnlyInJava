bf9c38a67847890624407a31906820cd7c745355
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-13 20:22:59 +0530

    Add files via upload

---
+++ simplePetternProgram.java
@@ -0,0 +1,13 @@
+package PatternInJava;
+
+public class simplePetternProgram {
+    public static void main(String [] args){
+        int num=5;
+        for(int i=1; i<num; i++){
+            for(int j=1; j<num; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
